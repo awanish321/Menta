359304905293fca8c63f568b8b6596c3ef7f7c70
awanish321 <awanish237@gmail.com>
2024-04-18 09:11:06 +0530

    Initial commit

---
+++ lib/Menta/constants/strings.dart
@@ -0,0 +1,68 @@
+// Industries
+const List<String> industries = [
+  'Software Development',
+  'Machine Learning',
+  'Devops',
+  'IT Security',
+  'Aviation',
+  'Engineering',
+  'Finance',
+  'Graphics & Design',
+];
+
+// Mentors
+const List<Map<String, dynamic>> mentors = [
+  {
+    'name': 'John K.',
+    'details': 'Finance Analyst | Gidstoops | Consultant | IIT Ahmedabad | Accounting | Project...',
+    'industry': 'Finance',
+    'experience': '2 Years',
+    'imageUrl': 'https://example.com/john.jpg',
+  },
+  {
+    'name': 'Priya Rai',
+    'details': 'Software Engineer | TCS Mumbai | Devops Expert | IIT Graduate',
+    'industry': 'IT',
+    'experience': '8 Years',
+    'imageUrl': 'https://example.com/priya.jpg',
+  },
+  {
+    'name': 'Rajesh Kumar',
+    'details': 'American Express | IIM Indore | Ex Tata Project | NITK Karnataka',
+    'industry': 'Finance',
+    'experience': '9 Years',
+    'imageUrl': 'https://example.com/rajesh.jpg',
+  },
+  {
+    'name': 'Susane Iyer',
+    'details': 'Software Engineer | TCS Mumbai | Devops Expert | IIT Graduate',
+    'industry': 'IT',
+    'experience': '12 Years',
+    'imageUrl': 'https://example.com/susane.jpg',
+  },
+];
+
+// Hot Skills
+const List<String> hotSkills = [
+  'Java',
+  'C++',
+  'C',
+  'AWS',
+  'NoSQL',
+  'SQL',
+];
+
+// Trending Jobs
+const List<Map<String, String>> trendingJobs = [
+  {'title': 'Software Development', 'iconUrl': 'https://example.com/software.png'},
+  {'title': 'Artificial Intelligence', 'iconUrl': 'https://example.com/ai.png'},
+  {'title': 'IT Security', 'iconUrl': 'https://example.com/security.png'},
+  {'title': 'Database', 'iconUrl': 'https://example.com/database.png'},
+  {'title': 'Devops', 'iconUrl': 'https://example.com/devops.png'},
+  {'title': 'Machine Learning', 'iconUrl': 'https://example.com/ml.png'},
+  {'title': 'Banking', 'iconUrl': 'https://example.com/banking.png'},
+  {'title': 'Finance', 'iconUrl': 'https://example.com/finance.png'},
+];
+
+
+
---
+++ lib/Menta/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
